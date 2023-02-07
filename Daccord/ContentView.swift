539d5462536de3ddacf1ded920052608a6515b9a
Kuan-Ting Lin <contact@ting.tw>
2023-02-08 04:42:22 +0800

    Initial Commit

---
+++ Daccord/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Daccord
+//
+//  Created by Kuan-Ting Lin on 2023/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daccord/DaccordApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaccordApp.swift
+//  Daccord
+//
+//  Created by Kuan-Ting Lin on 2023/2/8.
+//
+
+import SwiftUI
+
+@main
+struct DaccordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
