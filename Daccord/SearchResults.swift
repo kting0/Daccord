a39ee8a51c481886620484f625f06e4ef39dcff2
Kuan-Ting Lin <contact@ting.tw>
2023-02-08 20:10:18 +0800

    Made SearchResultItem a NavigationLink

--- Daccord/SearchResults.swift
+++ Daccord/SearchResults.swift
@@ -31,17 +31,19 @@ struct SearchResults_Previews: PreviewProvider {
 
 struct SearchResultItem: View {
     var body: some View {
-        HStack {
-            RoundedRectangle(cornerRadius: 2, style: .continuous)
-                            .fill(.gray)
-                            .frame(width: 50, height: 50)
-            VStack(alignment: .leading, spacing: 2) {
-                Text("Opium (feat. EARTHGANG)")
-                Text("Gorillaz - Song Machine, Season One: Strange Timez (Deluxe)")
-                    .foregroundColor(.secondary)
-                    .font(.footnote)
+        NavigationLink(destination: SongDetails()) {
+            HStack {
+                RoundedRectangle(cornerRadius: 2, style: .continuous)
+                                .fill(.gray)
+                                .frame(width: 50, height: 50)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Opium (feat. EARTHGANG)")
+                    Text("Gorillaz - Song Machine, Season One: Strange Timez (Deluxe)")
+                        .foregroundColor(.secondary)
+                        .font(.footnote)
+                }
+                .lineLimit(1)
             }
-            .lineLimit(1)
         }
         .alignmentGuide(.listRowSeparatorLeading) { _ in 0 }
     }
