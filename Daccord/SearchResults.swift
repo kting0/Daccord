c2feb0f6ee1a6a207d9ae1f9a47927b844076f3b
Kuan-Ting Lin <contact@ting.tw>
2023-02-08 19:53:33 +0800

    Added layouts for SearchResults

--- Daccord/SearchResults.swift
+++ Daccord/SearchResults.swift
@@ -9,12 +9,40 @@ import SwiftUI
 
 struct SearchResults: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section {
+                SearchResultItem()
+                SearchResultItem()
+                SearchResultItem()
+            }
+            .listSectionSeparator(.hidden)
+        }
+        
+        .listStyle(.plain)
     }
 }
 
 struct SearchResults_Previews: PreviewProvider {
     static var previews: some View {
         SearchResults()
+    }
+}
+
+
+struct SearchResultItem: View {
+    var body: some View {
+        HStack {
+            RoundedRectangle(cornerRadius: 2, style: .continuous)
+                            .fill(.gray)
+                            .frame(width: 50, height: 50)
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Opium (feat. EARTHGANG)")
+                Text("Gorillaz - Song Machine, Season One: Strange Timez (Deluxe)")
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+            }
+            .lineLimit(1)
+        }
+        .alignmentGuide(.listRowSeparatorLeading) { _ in 0 }
     }
 }
