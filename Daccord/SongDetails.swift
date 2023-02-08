8c911711f169cb8f16ec87acd3665af806d651ca
Kuan-Ting Lin <contact@ting.tw>
2023-02-08 08:01:41 +0800

    Added layouts for SongDetails

--- Daccord/SongDetails.swift
+++ Daccord/SongDetails.swift
@@ -9,7 +9,40 @@ import SwiftUI
 
 struct SongDetails: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            RoundedRectangle(cornerRadius: 10, style: .continuous)
+                            .fill(.gray)
+                            .frame(width: 250, height: 250)
+                            .shadow(color: .gray, radius: 15)
+            VStack(spacing: 5) {
+                Text("Opium (feat. EARTHGANG)")
+                    .bold()
+                Text("Gorillaz")
+                    .foregroundColor(.secondary)
+                + Text(Image(systemName: "chevron.forward"))
+                    .foregroundColor(.secondary)
+                Text("Song Machine, Season One: Strange Timez (Deluxe)")
+                    .foregroundColor(.secondary)
+                + Text(Image(systemName: "chevron.forward"))
+                    .foregroundColor(.secondary)
+            }
+            .font(.title3)
+            .multilineTextAlignment(.center)
+            HStack {
+                Button {
+                    
+                } label: {
+                    Label("Preview Song", systemImage: "play.fill")
+                }
+                .buttonStyle(.bordered)
+                .controlSize(.large)
+            }
+            Text("Released 2000/01/01")
+                .foregroundColor(.secondary)
+                .font(.footnote)
+                .bold()
+        }
+        .padding()
     }
 }
 
