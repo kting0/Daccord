a0762e84403bf6c1cb5dc922fe703c69807e4d27
Kuan-Ting Lin <contact@ting.tw>
2023-02-08 20:03:50 +0800

    Removed old Daccord.swift and point reference to old Search.swift. Also Change the variable searchText's @Binding attribute to @State

--- Daccord/Daccord.swift
+++ Daccord/Daccord.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Daccord.swift
 //  Daccord
 //
 //  Created by Kuan-Ting Lin on 2023/2/8.
@@ -8,19 +8,52 @@
 import SwiftUI
 
 struct Daccord: View {
+    @State var searchText: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                if searchText.isEmpty {
+                    List {
+                        Section(header: Text("Discover").font(.title2).bold().foregroundColor(.primary)) {
+                            Button {
+                                
+                            } label: {
+                                Text("ABAO")
+                                    .font(.title3)
+                                    .foregroundColor(.accentColor)
+                            }
+                            Button {
+                                
+                            } label: {
+                                Text("ABAO")
+                                    .font(.title3)
+                                    .foregroundColor(.accentColor)
+                            }
+                            Button {
+                                
+                            } label: {
+                                Text("ABAO")
+                                    .font(.title3)
+                                    .foregroundColor(.accentColor)
+                            }
+                        }
+                        .listSectionSeparator(.hidden)
+                    }
+                    .listStyle(.plain)
+                }
+                
+                if !searchText.isEmpty {
+                    SearchResults()
+                }
+            }
+            .navigationTitle("Search")
         }
-        .padding()
+        .searchable(text: $searchText, prompt: "Artists, Songs, Lyrics and More")
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Search_Previews: PreviewProvider {
     static var previews: some View {
-        Daccord()
+        Daccord(searchText: "")
     }
 }
